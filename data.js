70a697b0832438d492b94798e0d27855b4f609b7
leeeungyoung <92345613+leeeungyoung@users.noreply.github.com>
2022-06-26 15:06:30 +0900

    introduce data

---
+++ data.js
@@ -0,0 +1,15 @@
+let data = [
+    {
+      id: 0,
+      name: '김민준',
+      description: '리액트를 좋아하는 개발자',
+    },
+    {
+      id: 1,
+      name: '홍길동',
+      description: '고전 소설 홍길동전의 주인공',
+    }
+];
+
+export default data;
+
