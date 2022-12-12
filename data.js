7c2fc591e23edd6ebbb0a0b3dd52d84ae7753105
leeeungyoung <92345613+leeeungyoung@users.noreply.github.com>
2022-12-12 17:14:59 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 let data = [
     {
       id: 0,
-      name: '김민준',
-      description: '리액트를 좋아하는 개발자',
+      name: '쥴리',
+      description: '페르시안 화이트 클래식',
     },
     {
       id: 1,
-      name: '홍길동',
-      description: '고전 소설 홍길동전의 주인공',
+      name: '초코',
+      description: '코리안 숏헤어 치즈태비',
     }
 ];
 
